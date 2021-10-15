9cef7e758f6b5a608f0ebd5dad329b5a545db725
kris514 <90196066+kris514@users.noreply.github.com>
2021-10-15 17:24:03 +0300

    Add files via upload

--- .github/workflows/version_warning.py
+++ .github/workflows/version_warning.py
@@ -1,5 +1,9 @@
 from outdated import warn_if_outdated
+from outdated.mywarnings import OutdatedPackageWarning
 import qiskit
 import pyquil
-warn_if_outdated('qiskit', qiskit.__version__)
-warn_if_outdated('pyquil', pyquil.__version__)
+try:
+    warn_if_outdated('qiskit', qiskit.__version__, raise_exceptions=True)
+except OutdatedPackageWarning as warn:
+    print(warn)
+#warn_if_outdated('pyquil', pyquil.__version__)
