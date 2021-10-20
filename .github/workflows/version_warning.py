bd734c66b20703f1f2f2198ab1179dd4a4a742fa
kris514 <90196066+kris514@users.noreply.github.com>
2021-10-20 17:56:22 +0300

    Delete version_warning.py

--- .github/workflows/version_warning.py
+++
@@ -1,9 +0,0 @@
-from outdated import warn_if_outdated
-from outdated.mywarnings import OutdatedPackageWarning
-import qiskit
-import pyquil
-try:
-    warn_if_outdated('qiskit', qiskit.__version__, raise_exceptions=True)
-except OutdatedPackageWarning as warn:
-    print(warn)
-#warn_if_outdated('pyquil', pyquil.__version__)
